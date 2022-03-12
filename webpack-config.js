c70b618e04e4df0bd7b8ae0e9f288bf0c9e5b9ea
flaveyro <flavio.acg2019@gmail.com.com>
2022-03-12 15:20:43 -0600

    init project | webpack config

---
+++ webpack-config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/[hash].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      favicon: './public/assets/icons8-morty-smith.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style/[name].css',
+    }),
+  ],
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    static: './dist',
+    compress: true,
+    // port: 3000,
+    open: true,
+  },
+};
